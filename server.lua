3115a5a9d4524fd42b587970e9dedfa3cbe8d94a
ra1der <73917011+baranraider@users.noreply.github.com>
2022-12-04 18:21:49 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,4 +1,8 @@
---ESCKaybeden - ra1der
+-- Discord : ra1der#2112
+--discord.gg/wilddevelopment
+
+QBCore = nil
+TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
 
 AddEventHandler('playerDropped', function(reason)
     local konum = GetEntityCoords(GetPlayerPed(source))
@@ -21,12 +25,32 @@ AddEventHandler('playerDropped', function(reason)
             players.firstname = player[1].firstname
             players.lastname = player[1].lastname
         end
-    elseif Config.Framework == "NewQBCore" then 
-        local player = exports.oxmysql:executeSync("SELECT * FROM players WHERE citizenid = @citizenid", {['@citizenid'] = identifier})
-        if player[1] ~= nil then
-            players.firstname = player[1].firstname
-            players.lastname = player[1].lastname
-        end
     end
     TriggerClientEvent('setz_combatlogsend', -1, konum, players, identifier, os.date("%H:%M"), reason)
 end)
+
+--komut ile denemek isteyenler için;
+-- QBCore.Commands.Add('combatlog', 'Help Text', {}, false, function(source, args)
+--     local konum = GetEntityCoords(GetPlayerPed(source))
+--     local players = {}
+--     local identifier = ""
+--     if Config.identifier == "Steam" then
+--         identifier = GetPlayerIdentifier(source, 0)
+--     elseif Config.identifier == "License" then
+--         identifier = GetPlayerIdentifier(source, 1)
+--     end
+--     if Config.Framework == "ESX" then
+--         local player = exports.ghmattimysql:executeSync("SELECT * FROM users WHERE identifier = @identifier", {['@identifier'] = identifier})
+--         if player[1] ~= nil then
+--             players.firstname = player[1].firstname
+--             players.lastname = player[1].lastname
+--         end
+--     elseif Config.Framework == "QBCore" then
+--         local player = exports.ghmattimysql:executeSync("SELECT * FROM players WHERE steam = @steam", {['@steam'] = identifier})
+--         if player[1] ~= nil then
+--             players.firstname = player[1].firstname
+--             players.lastname = player[1].lastname
+--         end
+--     end
+--     TriggerClientEvent('setz_combatlogsend', -1, konum, players, identifier, os.date("%H:%M"))
+-- end)
